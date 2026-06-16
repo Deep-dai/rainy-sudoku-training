b2d2db3dea19bba99592452eac05b8f43d0f502a
Deep-dai <daibing1119@gmail.com>
2026-06-16 16:48:59 +0800

    Update Rainy Sudoku training app

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "rainy-sudoku-v5";
+const CACHE_NAME = "rainy-sudoku-v7";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=5",
-  "./scripts/app-state.js?v=5",
-  "./scripts/app-game.js?v=5",
-  "./scripts/app-ui.js?v=5",
-  "./scripts/app-rules.js?v=5",
-  "./scripts/app-utils.js?v=5",
+  "./styles.css?v=7",
+  "./scripts/app-state.js?v=7",
+  "./scripts/app-game.js?v=7",
+  "./scripts/app-ui.js?v=7",
+  "./scripts/app-rules.js?v=7",
+  "./scripts/app-utils.js?v=7",
   "./manifest.webmanifest",
   "./app-icon.svg"
 ];
